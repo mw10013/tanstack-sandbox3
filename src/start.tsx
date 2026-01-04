943b115af7ceb10ba3241fdc222c9876438c63fb
Michael Wu <mw10013@gmail.com>
2026-01-03 20:55:06 -0500

    refactor: remove authSessionMiddleware and related start instance logic

--- src/start.tsx
+++
@@ -1,8 +0,0 @@
-import { createStart } from "@tanstack/react-start";
-import { authSessionMiddleware } from "@/middleware/auth-session";
-
-export const startInstance = createStart(() => {
-  return {
-    requestMiddleware: [authSessionMiddleware],
-  };
-});
